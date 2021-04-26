f5386ff24423c8997bc2dff7883eb40418d48440
Abbas Al-Kaisi <fadox@users.noreply.github.com>
2021-04-26 05:18:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run 2021 *** Hey')
+    res.end('Hello Cloud Run 2021 *** CI & CD')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
